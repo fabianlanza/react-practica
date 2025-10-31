0d0e801cfda8fe42d781e31705060e3eadb0012a
fabianlanza <fabianlanza17@gmail.com>
2025-10-31 00:37:49 -0600

    Ajuste de NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,29 @@ function App() {
 
     return (
       <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <Link className="nav-link" to={"/"}>Inicio</Link>
-              <Link className="nav-link" to={"/clientes"}>Clientes</Link>
-              <Link className="nav-link" to={"/productos"}>Productos</Link>
-              <Link className="nav-link" to={"/acercade"}>Acerca de</Link>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top shadow-sm">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <Link className="nav-link" to={"/"}>Inicio</Link>
+                <Link className="nav-link" to={"/clientes"}>Clientes</Link>
+                <Link className="nav-link" to={"/productos"}>Productos</Link>
+                <Link className="nav-link" to={"/acercade"}>Acerca de</Link>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
 
-       <Routes>
-        <Route path = '/' element = {<Inicio />}/>
-        <Route path = '/clientes' element = {<Usuarios /> }/>
-        <Route path = '/productos' element = {<Productos /> }/>
-       </Routes>
+        <Routes>
+          <Route path = '/' element = {<Inicio />}/>
+          <Route path = '/clientes' element = {<Usuarios /> }/>
+          <Route path = '/productos' element = {<Productos /> }/>
+        </Routes>
       </>
     )
   } 
